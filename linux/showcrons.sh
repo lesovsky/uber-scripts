279c7d17820bc4e0a547e2362d5ed753f6987630
Alexey Lesovsky <lesovsky@gmail.com>
2014-02-17 12:55:34 +0600

    add linux/showcrons.sh

---
+++ linux/showcrons.sh
@@ -0,0 +1,15 @@
+#/usr/bin/env bash
+# Description:  Print all cron task
+# Comment:      To start with sudo use "sudo -iE /full/path/script_name"
+for c in /etc/crontab /etc/cron.*/* $(ls /var/spool/cron/crontabs/*)
+  do 
+    red='\e[0;31m'; NC='\e[0m'
+    if [[ $(head -n1 $c) == *#!* ]]
+      then
+        echo -e "${red}  $c${NC}: found she-bang, probably is a script."
+      else
+        echo -e "${red}  $c"${NC}
+        grep --color=never -E "^[0-9*]+" $c
+    fi
+    echo
+  done
