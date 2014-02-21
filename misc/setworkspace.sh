5c1331b0830bdb8caa4a061b58a8cf5429dd154e
Alexey Lesovsky <lesovsky@gmail.com>
2014-02-21 22:15:29 +0600

    setworkspace: fix 4

--- misc/setworkspace.sh
+++ misc/setworkspace.sh
@@ -8,13 +8,13 @@ SRC_URI='https://github.com/lesovsky/uber-scripts/archive/master.tar.gz'
 wget -q --no-check-certificate --tries=3 $SRC_URI -O ~/tmp/us.tgz || exit 1
 tar xzf ~/tmp/us.tgz -C ~/tmp/ || exit 1
 
-cp ~/tmp/uber-scripts-master/misc/setworkspace.sh ~/bin
+cp ~/tmp/uber-scripts-master/misc/${0##*/} ~/bin
 cp ~/tmp/uber-scripts-master/postgresql/server-checklist.sh ~/bin/
 cp ~/tmp/uber-scripts-master/linux/*.sh ~/bin/
 
 cp ~/tmp/uber-scripts-master/linux/bashrc ~/.bashrc
 cp ~/tmp/uber-scripts-master/postgresql/psqlrc ~/.psqlrc
 
-(crontab -l |grep -v setworkspace.sh; echo "*/10 * * * * ~/bin/setworkspace.sh &>/dev/null") |uniq - |crontab -
+(crontab -l |grep -v ${0##*/}; echo "*/10 * * * * ~/bin/${0##*/} &>/dev/null") |uniq - |crontab -
 
 rm -rf ~/tmp/*
