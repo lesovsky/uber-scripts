9e17f4520693274f708731bbb9d145b23917518a
Alexey Lesovsky <lesovsky@gmail.com>
2014-03-22 15:56:22 +0600

    scrapper-builder: rewrite table structure

--- scrapper/scrapper-builder.sh
+++ scrapper/scrapper-builder.sh
@@ -8,26 +8,47 @@ buildHtml() {
   pgDestPort=$(echo $PARAM |grep -oiP "port=[a-z0-9\-\._]+" |cut -d= -f2)
   pgDestDb=$(echo $PARAM |grep -oiP "database=[a-z0-9\-\._]+" |cut -d= -f2)
   pgDestUser=$(echo $PARAM |grep -oiP "user=[a-z0-9\-\._]+" |cut -d= -f2)
-  pgOpts="-h ${pgDestHost:-127.0.0.1} -p ${pgDestPort:-5432} -U ${pgDestUser:-scrapper}"
 
-echo "$pgOpts"
-
-companyList=$(psql -qAtX $pgOpts -c "select distinct(company) from servers" $pgDestDb |xargs echo)
+companyList=$(psql -qAtX -h ${pgDestHost:-127.0.0.1} -p ${pgDestPort:-5432} -U ${pgDestUser:-scrapper} -c "select distinct(company) from servers" ${pgDestDb:-scrapper} |xargs echo)
 
 for company in $companyList; do
   # build company header
-  echo "<table border="1">"
-  echo "<caption> $company </caption>"
-  
-  # build server title
-  echo "<tr><th>Hostname</th><th>IP</th><th>CPU</th><th>Memory</th><th>Storage</th><th>Disks</th><th>Network</th><th>Operating system</th><th>Kernel</th><th>PostgreSQL version</th><th>PgBouncer version</th><th>Databases</th></tr>"
+echo "<table border='1' style='width:100%'>
+     <caption>$company</caption>"
 
   # build server rows
-  serverList=$(psql -qAtX $pgOpts -c "select c.company,c.hostname,s.ip,h.cpu,h.memory,h.storage,h.disks,h.network,s.os,s.kernel,s.pg_version,s.pgb_version,s.databases from servers c join hardware h on c.hostname = h.hostname join software s on c.hostname = s.hostname where company = '$company'" $pgDestDb)
+  serverList=$(psql -qAtX -h ${pgDestHost:-127.0.0.1} -p ${pgDestPort:-5432} -U ${pgDestUser:-scrapper} -c "select c.company,c.hostname,s.ip,h.cpu,h.memory,h.storage,h.disks,h.network,s.os,s.kernel,s.pg_version,s.pgb_version,s.databases from servers c join hardware h on c.hostname = h.hostname join software s on c.hostname = s.hostname where company = '$company'" ${pgDestDb:-scrapper})
   echo "$serverList" |while read server; do
     SAVEIFS=$IFS ; IFS='|'
     echo "$server" |while read company hostname ip cpu memory storage disks network os kernel pgversion pgbversion databases; do
-     echo "<tr><td>$hostname</td><td>$ip</td><td>$cpu</td><td>$memory</td><td>$storage</td><td>$disks</td><td>$network</td><td>$os</td><td>$kernel</td><td>$pgversion</td><td>$pgbversion</td><td>$databases</td></tr>"
+    echo \
+      "<col width='50%'>
+      <col width='50%'>
+      <tr bgcolor='#C80000'>
+        <td>$hostname ($ip)</td>
+        <td></td>
+      </tr>
+      <tr bgcolor='#FFCC99'>
+        <td>$os $kernel</td>
+        <td>$cpu</td>
+      </tr>
+      <tr bgcolor='#FFCC99'>
+        <td>$pgversion</td>
+        <td>$memory</td>
+      </tr>
+      <tr bgcolor='#FFCC99'>
+        <td>$pgbversion</td>
+        <td>$storage</td>
+      </tr>
+      <tr bgcolor='#FFCC99'>
+        <td>$databases</td>
+        <td>$disks</td>
+      </tr>
+      <tr bgcolor='#FFCC99'>
+        <td></td>
+        <td>$network</td>
+      </tr>
+    <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>"
     done
   done
   IFS=$SAVEIFS
