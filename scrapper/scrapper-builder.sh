e07efb407869a20d7d52ff098d5c9d96c8297d5b
Alexey Lesovsky <lesovsky@gmail.com>
2014-03-22 10:33:52 +0600

    scrapper-builder: rewrite parameters part, wrap into functions

--- scrapper/scrapper-builder.sh
+++ scrapper/scrapper-builder.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 # Description:	some cool description here
 
-pgHost='127.0.0.1'
-pgPort='5432'
-pgDatabase='test'
-pgUser='postgres'
+PARAM="$@"
 
-companyList=$(psql -qAtX -h $pgHost -p $pgPort -U $pgUser -c "select distinct(company) from servers" $pgDatabase |xargs echo)
+buildHtml() {
+  pgDestHost=$(echo $PARAM |grep -oiP "host=[a-z0-9\-\._]+" |cut -d= -f2)
+  pgDestPort=$(echo $PARAM |grep -oiP "port=[a-z0-9\-\._]+" |cut -d= -f2)
+  pgDestDb=$(echo $PARAM |grep -oiP "database=[a-z0-9\-\._]+" |cut -d= -f2)
+  pgDestUser=$(echo $PARAM |grep -oiP "user=[a-z0-9\-\._]+" |cut -d= -f2)
+  pgOpts="-h ${pgDestHost:-127.0.0.1} -p ${pgDestPort:-5432} -U ${pgDestUser:-scrapper}"
+
+echo "$pgOpts"
+
+companyList=$(psql -qAtX $pgOpts -c "select distinct(company) from servers" $pgDestDb |xargs echo)
 
 for company in $companyList; do
   # build company header
@@ -17,7 +23,7 @@ for company in $companyList; do
   echo "<tr><th>Hostname</th><th>IP</th><th>CPU</th><th>Memory</th><th>Storage</th><th>Disks</th><th>Network</th><th>Operating system</th><th>Kernel</th><th>PostgreSQL version</th><th>PgBouncer version</th><th>Databases</th></tr>"
 
   # build server rows
-  serverList=$(psql -qAtX -h $pgHost -p $pgPort -U $pgUser -c "select c.company,c.hostname,s.ip,h.cpu,h.memory,h.storage,h.disks,h.network,s.os,s.kernel,s.pg_version,s.pgb_version,s.databases from servers c join hardware h on c.hostname = h.hostname join software s on c.hostname = s.hostname where company = '$company'" $pgDatabase)
+  serverList=$(psql -qAtX $pgOpts -c "select c.company,c.hostname,s.ip,h.cpu,h.memory,h.storage,h.disks,h.network,s.os,s.kernel,s.pg_version,s.pgb_version,s.databases from servers c join hardware h on c.hostname = h.hostname join software s on c.hostname = s.hostname where company = '$company'" $pgDestDb)
   echo "$serverList" |while read server; do
     SAVEIFS=$IFS ; IFS='|'
     echo "$server" |while read company hostname ip cpu memory storage disks network os kernel pgversion pgbversion databases; do
@@ -28,3 +34,13 @@ for company in $companyList; do
   # close table
   echo "</table>"
 done
+}
+
+main() {
+  case "$PARAM" in
+    --build=* ) buildHtml ;;
+    --usage|--help|* ) usage ;;
+  esac
+}
+
+main
