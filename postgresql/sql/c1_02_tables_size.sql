cf258e58909a700e3682627090d226866600586d
Alexey Lesovsky <lesovsky@gmail.com>
2014-05-05 13:11:15 +0600

    add index usage authored by J.Berkus

--- postgresql/sql/c1_02_tables_size.sql
+++ postgresql/sql/c1_02_tables_size.sql
@@ -14,7 +14,7 @@ where
 union all
 
 select 
-  'other' as relation,
+  '*.medium_size' as relation,
   NULL as column_count,
   (select count(*) from pg_index,pg_stat_user_tables where pg_index.indrelid=pg_stat_user_tables.relid) as index_count,
   pg_size_pretty(sum(pg_relation_size(relname::regclass))) as t_size,
@@ -22,7 +22,21 @@ select
   (sum(pg_indexes_size(relname::regclass) * 100) / sum(pg_relation_size(relname::regclass)))::numeric(20,2) as i_ratio 
 from pg_stat_user_tables
 where
-  pg_relation_size(relname::regclass) < 1000000000 -- less than 1GB
+  pg_relation_size(relname::regclass) < 1000000000
+  and pg_relation_size(relname::regclass) >= 300000000 -- more or equal than 300MB
+
+union all
+
+select 
+  '*.small_size' as relation,
+  NULL as column_count,
+  (select count(*) from pg_index,pg_stat_user_tables where pg_index.indrelid=pg_stat_user_tables.relid) as index_count,
+  pg_size_pretty(sum(pg_relation_size(relname::regclass))) as t_size,
+  pg_size_pretty(sum(pg_indexes_size(relname::regclass))) as i_size,
+  (sum(pg_indexes_size(relname::regclass) * 100) / sum(pg_relation_size(relname::regclass)))::numeric(20,2) as i_ratio 
+from pg_stat_user_tables
+where
+  pg_relation_size(relname::regclass) < 300000000 -- less than 300MB
 
 order by
-  t_size desc
+  column_count desc nulls last;
