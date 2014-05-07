5609855f5b54056c89eab685c746a9cd641e9c27
Alexey Lesovsky <lesovsky@gmail.com>
2014-05-07 10:40:13 +0600

    basebackup-validation.sh -- allow concurrent basebackup validation

--- misc/megafon-ural-service-guide.sh
+++ misc/megafon-ural-service-guide.sh
@@ -2,12 +2,6 @@
 # Description: Get balance info from Megafon Service Guide.
 
 export PATH="/bin:/usr/bin:/usr/local/bin"
-PARAM=$1
-
-shift 1
-
-login=$1
-password=$2
 
 print_usage() {
 local red='\e[0;31m'; NC='\e[0m'
@@ -24,7 +18,14 @@ Usage:
   --usage	print this help.
  phone_number	10-digits phone number (without country code).
  password	Service Guide password."
+exit 0
 }
+
+PARAM=$1
+shift 1
+
+login=$1
+password=$2
 
 response=$(curl -sk "https://uralsg.megafon.ru/ROBOTS/SC_TRAY_INFO?X_Username=$login&X_Password=$password")
 name=$(echo "$response" |grep "<NAME>" |head -n 1 |grep -oE ">.*<" |tr -d \<\>)
@@ -33,13 +34,13 @@ status=$(echo "$response" |grep "<STATUS>" |head -n 1 |grep -oE ">.*<" |tr -d \<
 balance=$(echo "$response" |grep "<BALANCE>" |head -n 1 |grep -oE ">.*<" |tr -d \<\>)
 plan=$(echo "$response" |grep "<PLAN_NAME>" |head -n 1 |grep -oE ">.*<" |tr -d \<\>)
 vTotal=$(echo "$response" |grep "<VOLUME_TOTAL>" |head -n 1 |grep -oE ">.*<" |tr -d \<\>)
-vAvailable=$(echo "$response" |grep "<VOLUME_AVAILABLE>" |head -n 1 |grep -oE ">.*<" |tr -d \<\>)
+vAvailable=$(echo "$response" |grep "<VOLUME_AVAILABLE>" |head -n 1 |grep -oE ">.*<" |tr -d \<\> |cut -d. -f1)
 vUsed=$(echo $(($vTotal - $vAvailable)))
 
 case $PARAM in
 --full )
 echo "$name (phone: $number status: $status)
-Plan: $plan, Total: $vTotal kB, Used: $vUsed kB, Available: $vAvailable kB.
+Plan: $plan, Total: $vTotal MB, Used: $vUsed MB, Available: $vAvailable MB.
 Balance: $balance RUR"
 ;;
 --balance ) echo $balance ;;
