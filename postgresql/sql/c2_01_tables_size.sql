7aa1f2c0334f9810a264c3b942a62a09ef1903df
Alexey Lesovsky <lesovsky@gmail.com>
2014-09-10 09:52:07 +0600

    postgresql/sql table size snippet: change relname to relid

--- postgresql/sql/c2_01_tables_size.sql
+++ postgresql/sql/c2_01_tables_size.sql
@@ -4,12 +4,12 @@ select
   schemaname||'.'||relname as relation,
   (select count(column_name) from information_schema.columns where table_name = relname) as column_count,
   (select count(*) from pg_index where pg_index.indrelid=pg_stat_user_tables.relid) as index_count,
-  pg_size_pretty(pg_relation_size(relname::regclass)) as t_size,
-  pg_size_pretty(pg_indexes_size(relname::regclass)) as i_size,
-  (pg_indexes_size(relname::regclass) * 100 / pg_relation_size(relname::regclass))::numeric(20,2) as i_ratio 
+  pg_size_pretty(pg_relation_size(relid)) as t_size,
+  pg_size_pretty(pg_indexes_size(relid)) as i_size,
+  (pg_indexes_size(relid) * 100 / pg_relation_size(relid))::numeric(20,2) as i_ratio 
 from pg_stat_user_tables
 where
-  pg_relation_size(relname::regclass) >= 1000000000 -- more or equal than 1GB
+  pg_relation_size(relid) >= 1000000000 -- more or equal than 1GB
 
 union all
 
@@ -17,13 +17,13 @@ select
   '*.medium_size' as relation,
   NULL as column_count,
   (select count(*) from pg_index,pg_stat_user_tables where pg_index.indrelid=pg_stat_user_tables.relid) as index_count,
-  pg_size_pretty(sum(pg_relation_size(relname::regclass))) as t_size,
-  pg_size_pretty(sum(pg_indexes_size(relname::regclass))) as i_size,
-  (sum(pg_indexes_size(relname::regclass) * 100) / sum(pg_relation_size(relname::regclass)))::numeric(20,2) as i_ratio 
+  pg_size_pretty(sum(pg_relation_size(relid))) as t_size,
+  pg_size_pretty(sum(pg_indexes_size(relid))) as i_size,
+  (sum(pg_indexes_size(relid) * 100) / sum(pg_relation_size(relid)))::numeric(20,2) as i_ratio 
 from pg_stat_user_tables
 where
-  pg_relation_size(relname::regclass) < 1000000000
-  and pg_relation_size(relname::regclass) >= 300000000 -- more or equal than 300MB
+  pg_relation_size(relid) < 1000000000
+  and pg_relation_size(relid) >= 300000000 -- more or equal than 300MB
 
 union all
 
@@ -31,12 +31,12 @@ select
   '*.small_size' as relation,
   NULL as column_count,
   (select count(*) from pg_index,pg_stat_user_tables where pg_index.indrelid=pg_stat_user_tables.relid) as index_count,
-  pg_size_pretty(sum(pg_relation_size(relname::regclass))) as t_size,
-  pg_size_pretty(sum(pg_indexes_size(relname::regclass))) as i_size,
-  (sum(pg_indexes_size(relname::regclass) * 100) / sum(pg_relation_size(relname::regclass)))::numeric(20,2) as i_ratio 
+  pg_size_pretty(sum(pg_relation_size(relid))) as t_size,
+  pg_size_pretty(sum(pg_indexes_size(relid))) as i_size,
+  (sum(pg_indexes_size(relid) * 100) / sum(pg_relation_size(relid)))::numeric(20,2) as i_ratio 
 from pg_stat_user_tables
 where
-  pg_relation_size(relname::regclass) < 300000000 -- less than 300MB
+  pg_relation_size(relid) < 300000000 -- less than 300MB
 
 order by
   column_count desc nulls last;
