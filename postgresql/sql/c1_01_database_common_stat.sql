6d140888fdeb28a8827117231261bb0d27c41750
Alexey Lesovsky <lesovsky@gmail.com>
2014-10-03 12:06:05 +0600

    sql fix

--- postgresql/sql/c1_01_database_common_stat.sql
+++ postgresql/sql/c1_01_database_common_stat.sql
@@ -9,5 +9,6 @@ SELECT
   round(((100 * tup_inserted + tup_updated + tup_deleted) / (tup_inserted + tup_updated + tup_deleted + tup_returned + tup_fetched)::numeric),3) as write_ratio,
   pg_size_pretty(pg_database_size(datname)),
   stats_reset 
-FROM pg_stat_database 
+FROM pg_stat_database
+WHERE (tup_inserted + tup_updated + tup_deleted + tup_returned + tup_fetched) > 0
 ORDER BY pg_database_size(datname) DESC;
