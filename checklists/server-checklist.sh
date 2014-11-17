262195d4aedb7cffcaac7eaa42582e71630170bf
Alexey Lesovsky <lesovsky@gmail.com>
2014-11-17 15:09:47 +0500

    checklist: add mounted fs and cpu scheduler checks

--- checklists/server-checklist.sh
+++ checklists/server-checklist.sh
@@ -2,64 +2,76 @@
 # Description:	Check and print server system parameters
 
 export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
 
+main() {
 # Memory & Swap
-echo "=== memory: free ===
-$(grep -E "^(Mem|Swap)Total:" /proc/meminfo |sed -e 's/^/   /g')"
+echo "${red}=== memory: status ===${reset}
+$(grep -E "^(Mem|Swap)Total:" /proc/meminfo)"
 
 # Virtual Memory checks
-echo "=== sysctl: virtual memory ===
-   $(sysctl vm.dirty_background_bytes)
-   $(sysctl vm.dirty_bytes)
-   $(sysctl vm.dirty_background_ratio)
-   $(sysctl vm.dirty_ratio)
-   $(sysctl vm.swappiness)
-   $(sysctl vm.overcommit_memory)
-   $(sysctl vm.overcommit_ratio)"
+echo "${red}=== sysctl: virtual memory ===${reset}
+$(sysctl -a --pattern 'vm.dirty.*(ratio|bytes)')
+$(sysctl vm.swappiness)
+$(sysctl -a --pattern 'vm.overcommit_(memory|ratio)')"
 
 # NUMA checks
-echo "=== sysctl: numa related ===
-   $(sysctl vm.zone_reclaim_mode)"
+echo "${red}=== sysctl: numa related ===${reset}
+$(sysctl vm.zone_reclaim_mode)"
+
+# CPU Scheduler check
+echo "${red}=== sysctl: cpu scheduler ===${reset}
+$(sysctl -a --pattern 'kernel.sched_(migration_cost|autogroup_enabled)')"
 
 # Files limits check
-echo "=== sysctl: files limits ===
-   $(sysctl fs.file-max)                        # maximum number of open files
-   $(sysctl fs.inotify.max_user_watches)        # maximum inotify watches per user
-   open files limit (ulimit -n): $(ulimit -n)          # maximum number of open files per process
-   available port range: $(sysctl net.ipv4.ip_local_port_range)"
+echo "${red}=== sysctl: files limits ===${reset}
+$(sysctl fs.file-max)                        # maximum number of open files
+$(sysctl fs.inotify.max_user_watches)        # maximum inotify watches per user
+open files limit (ulimit -n): $(ulimit -n)          # maximum number of open files per process
+available port range: $(sysctl net.ipv4.ip_local_port_range)"
 
 # Transparent Hugepages check
-echo "=== sysfs: transparent hugepages ===
-   /sys/kernel/mm/transparent_hugepage/enabled: $(cat /sys/kernel/mm/transparent_hugepage/enabled)
-   /sys/kernel/mm/transparent_hugepage/defrag: $(cat /sys/kernel/mm/transparent_hugepage/defrag)"
+echo "${red}=== sysfs: transparent hugepages ===${reset}
+/sys/kernel/mm/transparent_hugepage/enabled: $(cat /sys/kernel/mm/transparent_hugepage/enabled)
+/sys/kernel/mm/transparent_hugepage/defrag: $(cat /sys/kernel/mm/transparent_hugepage/defrag)"
 
 # Time check
-echo "=== Ntpd info ==="
+echo "${red}=== Ntpd info ===${reset}"
 if which ntpd &>/dev/null
   then 
     if [[ $(ps ho comm -C ntpd) == *ntpd* ]]
-       then echo "   Ntpd found and running."
-       else echo "   Ntpd found, but not running."
+       then echo "Ntpd found and running."
+       else echo "Ntpd found, but not running."
     fi
-  else echo "   Ntpd not found."
+  else echo "Ntpd not found."
 fi
-echo "=== Clocksource: available and current clocksource ===
-   /sys/devices/system/clocksource/clocksource0/available_clocksource: $(cat /sys/devices/system/clocksource/clocksource0/available_clocksource)
-   /sys/devices/system/clocksource/clocksource0/current_clocksource: $(cat /sys/devices/system/clocksource/clocksource0/current_clocksource)"
+echo "${red}=== Clocksource: available and current clocksource ===${reset}
+/sys/devices/system/clocksource/clocksource0/available_clocksource: $(cat /sys/devices/system/clocksource/clocksource0/available_clocksource)
+/sys/devices/system/clocksource/clocksource0/current_clocksource: $(cat /sys/devices/system/clocksource/clocksource0/current_clocksource)"
 
-echo "=== EDAC ==="
+echo "${red}=== EDAC ===${reset}"
 if [[ $(lsmod |grep edac) ]]
   then
     for i in $(ls /sys/devices/system/edac/mc/mc*/*e_count);
-      do echo "   $i - $(cat $i)";
+      do echo "$i - $(cat $i)";
     done
   else
-    echo "   edac modules not loaded"
+    echo "edac modules not loaded"
 fi
 
-echo "=== CPU Governor ==="
-echo "   current kernel version: $(uname -r)"
+# CPU Governor info
+echo "${red}=== CPU Governor ===${reset}
+current kernel version: $(uname -r)"
 for i in $(ls -1 /sys/devices/system/cpu/ | grep -oE 'cpu[0-9]+'); 
 do 
-  echo "   $i: $(cat /sys/devices/system/cpu/$i/cpufreq/scaling_governor) (driver: $(cat /sys/devices/system/cpu/$i/cpufreq/scaling_driver))";
+  echo "$i: $(cat /sys/devices/system/cpu/$i/cpufreq/scaling_governor) (driver: $(cat /sys/devices/system/cpu/$i/cpufreq/scaling_driver))";
 done;
+
+# Mounted filesystems
+echo "${red}=== Mounted filesystems ===${reset}"
+mount |grep -w -E 'ext(3|4)|reiserfs|xfs|rootfs' |column -t
+}
+
+main 
