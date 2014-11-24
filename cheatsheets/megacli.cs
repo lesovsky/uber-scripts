fff7a3bf19771eca505776d2e4dfc60fce9c3016
Alexey Lesovsky <lesovsky@gmail.com>
2014-11-24 18:25:19 +0700

    cheatsheets/megacli.cs add commands

--- cheatsheets/megacli.cs
+++ cheatsheets/megacli.cs
@@ -1,5 +1,9 @@
 MegaCli is a RAID utility For LSI MegaRAID Controllers
+LD - LogicalDrive
+PD - PhysicalDrive
+BBU - Backup Battery Unit
 
+megacli adpcount                # get controllers count
 megacli adpallinfo aall         # get information about all adapters
 megacli adpbbucmd aall          # get BBU information
 megacli ldinfo lall aall        # get LogicalDrives information from all adapters
@@ -7,5 +11,8 @@ magacli pdlist aall             # get PhysicalDrives information from all adapte
 
 megacli ldgetprop cache lall aall       # get LogicalDrives cache information 
 megacli ldgetprop dskcache lall aall    # get PhysicalDrive cache status
+MegaCli ldinfo lall aall |grep -E '(Virtual Drive|RAID Level|Cache Policy|Access Policy)'       # get LD cache and PD cache information
 
 megacli ldsetprop WB RA DisDskCache NoCachedBadBBU lall aall    # set WriteBack, Adaptive ReadAhead, disable Disk Cache, Disable Cache when bad BBU
+
+megacli adpfwflash -f filename aN       # flash adapter firmware from image
