911293509ae775e439bbe01b2262e82cc730a398
Alexey Lesovsky <lesovsky@gmail.com>
2014-12-16 23:42:36 +0500

    server-checklist.sh rewrite targets order, add storage io tuning target

--- checklists/server-checklist.sh
+++ checklists/server-checklist.sh
@@ -8,70 +8,50 @@ yellow=$(tput setaf 3)
 reset=$(tput sgr0)
 
 main() {
-# Memory & Swap
-echo "${yellow}=== memory: status ===${reset}
-$(grep -E "^(Mem|Swap)Total:" /proc/meminfo)"
+echo "${yellow}Tuning target: CPU scheduler${reset}"
+sysctl -e kernel.sched_migration_cost_ns kernel.sched_migration_cost kernel.sched_autogroup_enabled
 
-# Virtual Memory checks
-echo "${yellow}=== sysctl: virtual memory ===${reset}
-$(sysctl -e vm.dirty_background_bytes vm.dirty_bytes vm.dirty_background_ratio vm.dirty_ratio vm.swappiness vm.overcommit_memory vm.overcommit_ratio)"
+echo "${yellow}Tuning target: Virtual memory and NUMA${reset}"
+echo "$(grep -E "^(Mem|Swap)Total:" /proc/meminfo)"
+sysctl -e vm.dirty_background_bytes vm.dirty_bytes vm.dirty_background_ratio vm.dirty_ratio vm.swappiness vm.overcommit_memory vm.overcommit_ratio
 
-# NUMA checks
-echo "${yellow}=== sysctl: numa related ===${reset}
-available: $(ls -1d /sys/devices/system/node/node* |wc -l) nodes
-$(sysctl -e vm.zone_reclaim_mode)
-$(sysctl -e kernel.numa_balancing)"
+echo "NUMA node(s) available: $(ls -1d /sys/devices/system/node/node* |wc -l)"
+sysctl -e vm.zone_reclaim_mode kernel.numa_balancing
 
-# CPU Scheduler check
-echo "${yellow}=== sysctl: cpu scheduler ===${reset}
-$(sysctl -e kernel.sched_migration_cost_ns kernel.sched_migration_cost kernel.sched_autogroup_enabled)"
-
-# Files limits check
-echo "${yellow}=== sysctl: files limits ===${reset}
-$(sysctl -e fs.file-max)                        # maximum number of open files
-$(sysctl -e fs.inotify.max_user_watches)        # maximum inotify watches per user
-open files limit (ulimit -n): $(ulimit -n)          # maximum number of open files per process
-available port range: $(sysctl -e net.ipv4.ip_local_port_range)"
-
-# Transparent Hugepages check
-echo "${yellow}=== sysfs: transparent hugepages ===${reset}
-/sys/kernel/mm/transparent_hugepage/enabled: $(cat /sys/kernel/mm/transparent_hugepage/enabled)
+echo "${yellow}Tuning target: Huge pages${reset}"
+echo "/sys/kernel/mm/transparent_hugepage/enabled: $(cat /sys/kernel/mm/transparent_hugepage/enabled)
 /sys/kernel/mm/transparent_hugepage/defrag: $(cat /sys/kernel/mm/transparent_hugepage/defrag)"
 
-# Time check
-echo "${yellow}=== Ntpd info ===${reset}"
-if which ntpd &>/dev/null
-  then 
-    if [[ $(ps ho comm -C ntpd) == *ntpd* ]]
-       then echo "Ntpd found and running."
-       else echo "Ntpd found, but not running."
-    fi
-  else echo "Ntpd not found."
+echo "${yellow}Tuning target: File systems${reset}"
+mount |grep -w -E 'ext(3|4)|reiserfs|xfs|rootfs' |column -t
+
+echo "${yellow}Tuning target: Storage IO${reset}"
+if [ -d /sys/block/ ]
+  then
+    for i in $(ls -1 /sys/block/ | grep -oE 'sd[a-z]');
+      do
+        echo "$i: rotational: $(cat /sys/block/$i/queue/rotational); \
+        scheduler: $(cat /sys/block/$i/queue/scheduler); \
+        nr_requests: $(cat /sys/block/$i/queue/nr_requests); \
+        rq_affinity: $(cat /sys/block/$i/queue/rq_affinity); \
+        read_ahead_kb: $(cat /sys/block/$i/queue/read_ahead_kb)";
+      done #| awk '!(NR%2){print p "\t\t\t" $0}{p=$0}'
+    else
+      echo "/sys/block directory not found."
 fi
 
-# Pgbouncer open file limit
-echo "${yellow}=== Pgbouncer info ===${reset}"
-if pgrep pgbouncer &>/dev/null
-  then echo "pgbouncer open files limit: $(awk '/Max open files/{print "soft: " $4 " hard: " $5}' /proc/$(pgrep pgbouncer)/limits)"
-  else echo "pgbouncer not running"
-fi
+echo "${yellow}Tuning target: Networking${reset}"
+sysctl -e net.ipv4.ip_local_port_range
 
-echo "${yellow}=== Clocksource: available and current clocksource ===${reset}
-/sys/devices/system/clocksource/clocksource0/available_clocksource: $(cat /sys/devices/system/clocksource/clocksource0/available_clocksource)
+echo "${yellow}Tuning target: OS limits${reset}"
+sysctl -e fs.file-max fs.inotify.max_user_watches
+echo "open files limit (ulimit -n): $(ulimit -n)"
+
+echo "${yellow}Tuning target: Clocksource${reset}"
+echo "/sys/devices/system/clocksource/clocksource0/available_clocksource: $(cat /sys/devices/system/clocksource/clocksource0/available_clocksource)
 /sys/devices/system/clocksource/clocksource0/current_clocksource: $(cat /sys/devices/system/clocksource/clocksource0/current_clocksource)"
 
-echo "${yellow}=== EDAC ===${reset}"
-if [[ $(lsmod |grep edac) ]]
-  then
-    for i in $(ls /sys/devices/system/edac/mc/mc*/*e_count);
-      do echo "$i - $(cat $i)";
-    done
-  else
-    echo "edac modules not loaded"
-fi
-
-# CPU Governor
-echo "${yellow}=== CPU Governor ===${reset}"
+echo "${yellow}Tuning target: Power saving policy${reset}"
 if [ -d /sys/devices/system/cpu/cpu0/cpufreq/ ]
   then
     echo "current kernel version: $(uname -r)"
@@ -84,9 +64,35 @@ if [ -d /sys/devices/system/cpu/cpu0/cpufreq/ ]
       lscpu |grep -E '^(Model|Vendor|CPU( min| max)? MHz)'
 fi
 
-# Mounted filesystems
-echo "${yellow}=== Mounted filesystems ===${reset}"
-mount |grep -w -E 'ext(3|4)|reiserfs|xfs|rootfs' |column -t
+echo "${yellow}Tuning target: Services${reset}"
+if which ntpd &>/dev/null
+  then 
+    if [[ $(ps ho comm -C ntpd) == *ntpd* ]]
+       then echo "1. Ntpd found and running."
+       else echo "1. Ntpd found, but not running."
+    fi
+  else echo "1. Ntpd not found."
+fi
+
+if pgrep pgbouncer &>/dev/null
+  then
+    echo "2. Pgbouncer"
+      for i in $(pgrep pgbouncer); do 
+        echo "  pid: $i open files limit: $(awk '/Max open files/{print "soft: " $4 " hard: " $5}' /proc/$i/limits)"
+      done
+  else echo "2. Pgbouncer not running, skip."
+fi
+
+echo "${yellow}Tuning target: Miscellaneous${reset}"
+if [[ $(lsmod |grep edac) ]]
+  then
+    echo "1. Error Detection and Correction Module (EDAC)."
+    for i in $(ls /sys/devices/system/edac/mc/mc*/*e_count);
+      do echo "$i - $(cat $i)";
+    done
+  else
+    echo "1. Error Detection and Correction Module (EDAC) modules not loaded"
+fi
 }
 
 main 
