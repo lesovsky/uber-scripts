1fc9a7b4e8a994a377bdc08492230c49fea7fe2c
Alexey Lesovsky <lesovsky@gmail.com>
2015-01-23 21:12:30 +0500

    add checklists/errors-checklist.sh todo

---
+++ checklists/errors-checklist.sh
@@ -0,0 +1,5 @@
+# todo and ideas
+1. EDAC memory and PCI errors counters
+2. Ext4 sysfs error counters -- since 3.18 -- http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=52c198c6820f68b6fbe1d83f76e34a82bf736024
+3. Network interfaces error counters
+4. IPMI SEL (list/elist)
