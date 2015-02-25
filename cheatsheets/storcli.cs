af8798e5ca2fb29bcd6ac7ec3c515bd972a286f8
Alexey Lesovsky <lesovsky@gmail.com>
2015-02-25 15:20:42 +0500

    storcli.cs edit

--- cheatsheets/storcli.cs
+++ cheatsheets/storcli.cs
@@ -1,10 +1,19 @@
 StorCli is a RAID utility For LSI MegaRAID Controllers
-cx - controller
-vx - volumegroup (RAID)
-dx - diskgroup
+cx - controller, for example c0
+vx - volumegroup (RAID), for example v0
 
-# storcli /cx/vx show all                       show volumegroup properties
-# storcli /cx/vx set wrcache=WT|WB|AWB          array cache policy
-# storcli /cx/vx set rdcache=RA|NoRA            readahead policy
-# storcli /cx/vx set pdcache=On|Off|Default     disc cache policy
-# storcli /cx/bbu show                          bbu status
+# storcli cx show help                          show controller related help
+# storcli vx show help                          show virtual drive related help
+
+Troubleshoot
+# storcli /c0 show health                       show controller current health and status
+# storcli /c0 show termlog                      show firmware logs (very cryptic)
+# storcli /c0/bbu show                          bbu status
+
+Describe configuration
+# storcli /c0 show                              show controller info, vd list, pd list, cachevault status
+# storcli /c0/v0 show all                       show volumegroup properties
+# storcli /c0/v0 set wrcache=WT|WB|AWB          array cache policy
+# storcli /c0/v0 set rdcache=RA|NoRA            readahead policy
+# storcli /c0/v0 set pdcache=On|Off|Default     disc cache policy
+# storcli /c0/bbu show                          bbu status
