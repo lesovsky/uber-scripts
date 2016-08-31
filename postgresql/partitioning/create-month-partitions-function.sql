c3c1c7b18a60a7487fe82f317722f82bb2c3fe90
Alexey Lesovsky <lesovsky@gmail.com>
2016-08-31 15:05:08 +0500

    partitioning: fix func names

--- postgresql/partitioning/create-month-partitions-function.sql
+++ postgresql/partitioning/create-month-partitions-function.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION create_partitions(part TEXT , tblspace TEXT, init INT DEFAULT 0 ) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION create_monthly_partitions(part TEXT , tblspace TEXT, init INT DEFAULT 0 ) RETURNS VOID AS $$
 DECLARE
   current_check TIMESTAMP;
   next_check TIMESTAMP;
