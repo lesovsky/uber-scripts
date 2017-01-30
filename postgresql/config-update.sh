3f78a0a5790ff8862a137e47374712d6252e4040
Alexey Lesovsky <lesovsky@gmail.com>
2017-01-30 13:38:27 +0500

    config-update.sh: add usage note, change output colors

--- postgresql/config-update.sh
+++ postgresql/config-update.sh
@@ -3,9 +3,12 @@
 # Usage:        update-config.sh source_config destination_config
 # Author: Lesovsky A.V.
 
+[[ $# -lt 2 ]] && { echo -e "Usage:\n  $0 source.conf destination.conf"; exit 1; }
+
 srcCfg=$1
 destCfg=$2
 red=$(tput setaf 1)
+green=$(tput setaf 2)
 yellow=$(tput setaf 3)
 reset=$(tput sgr0)
 
@@ -15,20 +18,20 @@ reset=$(tput sgr0)
 export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
 
 # make a backup of dest config
-echo "${yellow}Backup $destCfg to $destCfg.backup${reset}"
+echo "${green}INFO:${reset} backup $destCfg to $destCfg.backup"
 cp $destCfg $destCfg.backup
 
-echo "${yellow}Processing $destCfg${reset}"
+echo "${green}INFO:${reset} processing $destCfg"
 grep -oE "^[a-z_\.]+ = ('.*'|[a-z0-9A-Z._-]+)" $srcCfg |while read guc ravno value;
   do
       if [[ $(grep -c -w $guc $destCfg) -eq 0 ]]; then
-          echo "WARNING: $destCfg doesn't contain ${red}$guc${reset} (value: $value)"
+          echo "${yellow}WARNING:${reset} $destCfg doesn't contain ${red}$guc${reset} (value: $value)"
       else
         sed -r -i -e "s|#?$guc = ('.*'\|[a-z0-9A-Z._-]+)|$guc = $value|g" $destCfg || echo "${red}ERROR:${reset} sed failed procssing: $guc = $value"
       fi
   done
 
-echo "${yellow}Done. Don't forget to fix parameters with version-specific values like:${reset}"
+echo "${green}Done.${reset} Don't forget to fix parameters with version-specific values like:"
 grep -oE "^[a-z_\.]+ = ('.*'|[a-z0-9A-Z._-]+)" $srcCfg |while read guc ravno value;
   do
       if [[ $value =~ (8|9|10)\.[0-9]{1} ]]; then
