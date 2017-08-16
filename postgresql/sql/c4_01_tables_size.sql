530b9e4bacd3164be69fd2352d644855f2d8014e
Aleksei Lesovskii <alesovskii@spotware.com>
2017-08-16 16:34:44 +0300

    postrgesql: rewrite tablesizes sql

--- postgresql/sql/c4_01_tables_size.sql
+++ postgresql/sql/c4_01_tables_size.sql
@@ -1,42 +1,12 @@
 -- Database structure checklist.
 -- Show tables size distribution.
 select 
-  schemaname||'.'||relname as relation,
-  (select count(column_name) from information_schema.columns where table_name = relname) as column_count,
-  (select count(*) from pg_index where pg_index.indrelid=pg_stat_user_tables.relid) as index_count,
-  pg_size_pretty(pg_relation_size(relid)) as t_size,
-  pg_size_pretty(pg_indexes_size(relid)) as i_size,
-  (pg_indexes_size(relid) * 100 / pg_relation_size(relid))::numeric(20,2) as i_ratio 
-from pg_stat_user_tables
-where
-  pg_relation_size(relid) >= 1000000000 -- more or equal than 1GB
-
-union all
-
-select 
-  '*.medium_size' as relation,
-  NULL as column_count,
-  (select count(*) from pg_index,pg_stat_user_tables where pg_index.indrelid=pg_stat_user_tables.relid) as index_count,
-  pg_size_pretty(sum(pg_relation_size(relid))) as t_size,
-  pg_size_pretty(sum(pg_indexes_size(relid))) as i_size,
-  (sum(pg_indexes_size(relid) * 100) / sum(pg_relation_size(relid)))::numeric(20,2) as i_ratio 
-from pg_stat_user_tables
-where
-  pg_relation_size(relid) < 1000000000
-  and pg_relation_size(relid) >= 300000000 -- more or equal than 300MB
-
-union all
-
-select 
-  '*.small_size' as relation,
-  NULL as column_count,
-  (select count(*) from pg_index,pg_stat_user_tables where pg_index.indrelid=pg_stat_user_tables.relid) as index_count,
-  pg_size_pretty(sum(pg_relation_size(relid))) as t_size,
-  pg_size_pretty(sum(pg_indexes_size(relid))) as i_size,
-  (sum(pg_indexes_size(relid) * 100) / sum(pg_relation_size(relid)))::numeric(20,2) as i_ratio 
-from pg_stat_user_tables
-where
-  pg_relation_size(relid) < 300000000 -- less than 300MB
-
-order by
-  column_count desc nulls last;
+	coalesce(t.spcname, 'pg_default') as tablespace,
+	n.nspname ||'.'||c.relname as table,
+	(select count(*) from pg_index i where i.indrelid=c.oid) as index_count,
+	pg_size_pretty(pg_relation_size(c.oid)) as t_size,
+	pg_size_pretty(pg_indexes_size(c.oid)) as i_size
+from pg_class c
+join pg_namespace n on c.relnamespace = n.oid
+left join pg_tablespace t on c.reltablespace = t.oid
+order by (pg_relation_size(c.oid),pg_indexes_size(c.oid)) desc;
