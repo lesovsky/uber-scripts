6413be12e75502500cb5f1165899877cab1d7a99
Alexey Lesovsky <lesovsky@gmail.com>
2018-06-07 21:33:30 +0500

    replication_activity_slots.sql: join pg_stat_activity

--- postgresql/sql/replication_activity_slots.sql
+++ postgresql/sql/replication_activity_slots.sql
@@ -1,13 +1,18 @@
 SELECT
-    slot_name,
-    slot_type,
-    plugin,
-    database,
-    active,
-    active_pid,
-    xmin,
-    age(catalog_xmin) as xmin_horizon,
-    pg_size_pretty(pg_xlog_location_diff(pg_current_xlog_location(), restart_lsn)) as restart_lsn_diff,
-    pg_size_pretty(pg_xlog_location_diff(pg_current_xlog_location(), confirmed_flush_lsn)) not_received
-FROM pg_replication_slots
-ORDER BY xmin_horizon DESC;
+    r.slot_name,
+    r.slot_type,
+    r.plugin,
+    r.database,
+    a.usename,
+    a.client_addr,
+    a.wait_event_type ||'.'|| a.wait_event AS await,
+    a.state,
+    r.active,
+    r.active_pid,
+    coalesce(r.catalog_xmin,'NULL') ||'/'|| coalesce(r.xmin,'NULL') as horizon,
+    greatest(age(r.catalog_xmin),age(r.xmin)) as horizon_age,
+    pg_size_pretty(pg_xlog_location_diff(pg_current_xlog_location(), r.restart_lsn)) as restart_lsn_diff,
+    pg_size_pretty(pg_xlog_location_diff(pg_current_xlog_location(), r.confirmed_flush_lsn)) not_received
+FROM pg_replication_slots r
+LEFT JOIN pg_stat_activity a ON r.active_pid = a.pid
+ORDER BY horizon_age DESC;
