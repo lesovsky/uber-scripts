acfb5bba3f719e87398dd3a43ac99c25233a2e58
Alexey Lesovsky <lesovsky@gmail.com>
2018-06-07 20:33:42 +0500

    replication_activity_slots.sql: rename columns, add desc order

--- postgresql/sql/replication_activity_slots.sql
+++ postgresql/sql/replication_activity_slots.sql
@@ -7,7 +7,7 @@ SELECT
     active_pid,
     xmin,
     age(catalog_xmin) as xmin_horizon,
-    pg_size_pretty(pg_xlog_location_diff(pg_current_xlog_location(), restart_lsn)) as accumulated_wal,
-    pg_size_pretty(pg_xlog_location_diff(pg_current_xlog_location(), confirmed_flush_lsn)) last_confirmed_flush
+    pg_size_pretty(pg_xlog_location_diff(pg_current_xlog_location(), restart_lsn)) as restart_lsn_diff,
+    pg_size_pretty(pg_xlog_location_diff(pg_current_xlog_location(), confirmed_flush_lsn)) not_received
 FROM pg_replication_slots
-ORDER BY xmin_horizon;
+ORDER BY xmin_horizon DESC;
